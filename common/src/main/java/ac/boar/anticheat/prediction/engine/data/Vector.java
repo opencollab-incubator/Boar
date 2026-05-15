dad13a3901a0dddca5569392c68ae0c77f9ae8bd
ethaniccc <benjamin@ziax.com>
2026-05-15 00:27:46 -0400

    remove Vector references to ack split

--- common/src/main/java/ac/boar/anticheat/prediction/engine/data/Vector.java
+++ common/src/main/java/ac/boar/anticheat/prediction/engine/data/Vector.java
@@ -11,21 +11,11 @@ import lombok.ToString;
 public class Vector {
     public final static Vector NONE = new Vector(VectorType.NORMAL, Vec3.ZERO);
 
-    private long stackId = -1;
-
     private Vec3 velocity;
     private VectorType type;
-    private boolean ignored;
-    private boolean twice;
 
     public Vector(final VectorType type, final Vec3 vec3) {
         this.type = type;
         this.velocity = vec3;
-    }
-
-    public Vector(final VectorType type, final Vec3 vec3, final long stackId) {
-        this.type = type;
-        this.velocity = vec3;
-        this.stackId = stackId;
     }
 }
