1b073b9cdd9e0aa3f1c5a1530afe1f897726ba7b
oryxel1 <gamingnoob681@gmail.com>
2026-05-28 15:55:25 +0700

    Minor change to chunk radius unloading.

--- src/main/java/ac/boar/anticheat/compensated/world/base/CompensatedWorld.java
+++ src/main/java/ac/boar/anticheat/compensated/world/base/CompensatedWorld.java
@@ -86,7 +86,7 @@ public class CompensatedWorld {
     }
 
     public boolean isOutOfRadius(int chunkX, int chunkZ) {
-        Vec3 radiusCenter = new Vec3(this.radiusCenter).add(0.5f, 0.5f, 0.5f); // Properly correct eh?
+        Vec3 radiusCenter = new Vec3(this.radiusCenter.getX(), 0, this.radiusCenter.getZ());
 
         // Still unsure about this... should we get rid of chunk sections, or chunk?
         // Well since we're getting rid of chunks for now, let set the y pos to 0.
